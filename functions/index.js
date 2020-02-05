fcc3c618e1b0fab50220adf139ba24b25230ea74
Weilon Ying <weilony@hotmail.com>
2020-02-05 08:36:28 +0000

    Added verification code generation and email sending

--- functions/index.js
+++ functions/index.js
@@ -27,6 +27,12 @@ sgClient.setApiKey(functions.config().sendgrid.api_key);
 sgClient.setDefaultRequest('baseUrl', 'https://api.sendgrid.com/');
 
 async function sendEmailToNewMember(user_id, data) {
+    // Try UNSW email first
+    let to_email = data.unsw_email;
+    if (!to_email) {
+        to_email = data.email;
+    }
+    console.log(to_email);
     const email_data = {
         "from": {
             "email": `${functions.config().sendgrid.from_email}`,
@@ -36,7 +42,7 @@ async function sendEmailToNewMember(user_id, data) {
             {
                 "to": [
                     {
-                        "email": `${data.email}`
+                        "email": `${to_email}`
                     }
                 ],
                 "dynamic_template_data": {
@@ -63,6 +69,13 @@ async function sendEmailToNewMember(user_id, data) {
         `Got back response ${response}`);
 }
 
+/* Add verification code to new member and fire off an email
+ * The following must be provided in the doc data:
+ * @param first_name            Passed to verification email
+ * @param last_name             Passed to verification email
+ * @param minecraft_username    Passed to verification email
+ * @param email OR unsw_email   So that we have an email to send to
+ */
 exports.onNewMember = functions.region(default_region)
     .firestore
     .document(`${default_collection}/{userID}`)
@@ -72,13 +85,76 @@ exports.onNewMember = functions.region(default_region)
         const verification_code = uuidv4();
         console.log(`Got document ${id}`);
 
+        if (!data.email && !data.unsw_email) {
+            throw new Error("Both fields 'email' and 'unsw_email' are empty!");
+        }
+
         // Add verification status and code to the document
         data.is_verified = false;
         data.verification_code = verification_code;
-        //doc.set(data);
         db.collection(default_collection).doc(id).set(data);
 
         // Then fire off an email!
         await sendEmailToNewMember(id, data);
         return;
+    });
+
+// HTTPS API endpoint to verify user.
+// Content-Type should be application/json
+// JSON should be like this:
+// {
+//     "user_id": <string>,
+//     "verification_code": <string>,
+//     "discord_id": <string>
+// }
+//
+// Returns JSON like this:
+// {
+//     "is_verified": <boolean>
+// }
+exports.verifyUser = functions
+    .region(default_region)
+    .https.onRequest(async (req, res) => {
+        if (req.method !== 'POST') {
+            return res.status(405).send('Incorrect method');
+        }
+
+        console.log('Received verification request. Request body: ', req.body);
+        const user_id = req.body.user_id;
+        const verification_code = req.body.verification_code;
+        const discord_id = req.body.discord_id;
+
+        if (!user_id || !verification_code || !discord_id) {
+            return res.status(400).send('Invalid data provided');
+        }
+        const userRef = db.collection(default_collection).doc(user_id);
+
+        try {
+            const userDoc = await userRef.get();
+            if (!userDoc.exists) {
+                return res.status(401).send('User does not exist');
+            }
+
+            const userData = userDoc.data();
+
+            // If user is already verified, skip this and return their verification status
+            if (!userData.is_verified) {
+                if (verification_code === userData.verification_code) {
+                    userData.is_verified = true;
+                    userData.discord_id = discord_id;
+                    db.collection(default_collection).doc(user_id).set(userData);
+                }
+            }
+            res.setHeader('Content-Type', 'application/json');
+            return res.json({"is_verified": userData.is_verified});
+        } catch (err) {
+            console.log('Error getting document', err);
+            return res.status(500).send('Internal server error');
+        }
+
+        /* WARNING: Remember to ensure that this function will always return a response!
+         * To test, uncomment the line below and run the linter. If linter says the line
+         * below is unreachable, you may re-comment it and continue to deploy
+         */
+        // return res.status(500).send('Internal server error');
     });
